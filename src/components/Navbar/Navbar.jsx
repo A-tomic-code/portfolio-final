975ae95d4cc3ca38f2ca127ab4e0f1f06d77bca7
atomic-code <danielvazquezheredia1994@gmail.com>
2023-05-08 16:26:46 +0200

    accion en los botones de la navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,58 +6,80 @@ export const Navbar = () => {
 
   const navLinks = [
     {
-      label:'Contáctame',
-      onClick: () => alert('oli')
+      label: 'Contáctame',
+      onClick: () =>
+        (window.location.href = 'mailto:danielvazquezheredia1994@gmail.com'),
     },
     {
-      label:'Visita mi LinkedIn',
-      onClick: () => alert('oli')
+      label: 'Visita mi LinkedIn',
+      onClick: () =>
+        (window.location.href = 'https://www.linkedin.com/in/atomic-code/'),
     },
     {
-      label:'Ver mi CV',
-      onClick: () => alert('oli')
-    }
+      label: 'Ver mi CV',
+      onClick: () =>
+        (window.location.href =
+          'https://drive.google.com/file/d/10DMZGwyldC51ZQJ6YJSSThLEBT-haazk/view?usp=sharing'),
+    },
   ];
   const collapsed__button = (
     <ul className="collapsed__button">
-        <button onClick={() => setShowCollapsedMenu(!showCollapsedMenu)}>
-
-            <div className={showCollapsedMenu ? 'collapsed_button__line collapsed_button__line--closed' : 'collapsed_button__line'}/>
-          <div className={showCollapsedMenu ? 'collapsed_button__line collapsed_button__line--closed' : 'collapsed_button__line'}/>
-          <div className={showCollapsedMenu ? 'collapsed_button__line collapsed_button__line--closed' : 'collapsed_button__line'}/>
-          
-        </button>
-      </ul>
-  )
-
+      <button onClick={() => setShowCollapsedMenu(!showCollapsedMenu)}>
+        <div
+          className={
+            showCollapsedMenu
+              ? 'collapsed_button__line collapsed_button__line--closed'
+              : 'collapsed_button__line'
+          }
+        />
+        <div
+          className={
+            showCollapsedMenu
+              ? 'collapsed_button__line collapsed_button__line--closed'
+              : 'collapsed_button__line'
+          }
+        />
+        <div
+          className={
+            showCollapsedMenu
+              ? 'collapsed_button__line collapsed_button__line--closed'
+              : 'collapsed_button__line'
+          }
+        />
+      </button>
+    </ul>
+  );
   const collapsed__menu = (
-    <ul className={`collapsed__menu ${showCollapsedMenu ? 'collapsed__menu--open' : ''}`}>
-          {navLinks.map((link, index) => {
-            return (
-              <li className="nav__link" key={index}>
-                <a href="#" onClick={link.onClick}>{link.label}</a>
-              </li>
-            );
-          })}
-        </ul>
-  )
-
+    <ul
+      className={`collapsed__menu ${
+        showCollapsedMenu ? 'collapsed__menu--open' : ''
+      }`}
+    >
+      {navLinks.map((link, index) => {
+        return (
+          <li className="nav__link" key={index}>
+            <a href="#" onClick={link.onClick}>
+              {link.label}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
   const expanded = (
     <ul className="expanded">
-        {navLinks.map((link, index) => {
-          return (
-            <li className="nav__link" key={index}>
-              <button onClick={link.onClick}>{link.label}</button>
-            </li>
-          );
-        })}
-      </ul>
-  )
-
-
+      {navLinks.map((link, index) => {
+        return (
+          <li className="nav__link" key={index}>
+            <button onClick={link.onClick}>{link.label}</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
   return (
-    <nav>    
+    <nav>
       {expanded}
       {collapsed__button}
       {collapsed__menu}
